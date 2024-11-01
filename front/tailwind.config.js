ada6da1781233a8b44561f34a7bdf61b081dac61
Noveled <noveled@naver.com>
2024-11-01 10:16:51 +0900

    폰트 색상 설정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text_d_main: '#1E1E1E',      // font-main_black
+        text_d_second: '#757575',    // font-gray
+      },
+    },
   },
   plugins: [],
 }
